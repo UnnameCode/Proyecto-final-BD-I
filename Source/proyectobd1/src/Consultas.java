c14cffded55e6113192deec46fc7fa8f07ab33af
UnnameCode <juan.figueroa@uao.edu.co>
2021-11-10 14:46:27 -0800

    otra

--- Source/proyectobd1/src/Consultas.java
+++ Source/proyectobd1/src/Consultas.java
@@ -14,63 +14,63 @@ public class Consultas {
    static final String USER = "root";
    static final String PASS = "Premiun123";
    
-    public Consultas(String[] args){
-        
-        try(Connection bd_projectfinal = DriverManager.getConnection(DB_URL, USER, PASS); 
-                Statement stmt = bd_projectfinal.createStatement();
-        ) {		      
-         System.out.println("consultar los contratos vigentes de un cliente");          
-         String sql = "SELECT numero_contrato, tipo_persona, tipo_uso FROM propietario WHERE fecha_finalizacion > curdate() AND numero_contrato = (SELECT numero_contrato_propietario FROM contrato WHERE numero_contrato = (SELECT numero_contrato FROM persona));\n" +
-"SELECT numero_contrato, seguro_mecanica, fecha_finalizacion, frecuencia_pago, fecha_poliza, fecha_terminacion_poliza FROM membresia WHERE fecha_finalizacion > curdate() AND numero_contrato = (SELECT numero_contrato_membresia FROM contrato WHERE numero_contrato = (SELECT numero_contrato FROM persona));\n" +
-"SELECT numero_contrato, fecha_inicio, fecha_finalizacion, frecuencia_pago, poliza_amparo, local_asignado FROM membresia WHERE fecha_finalizacion > curdate() AND numero_contrato = (SELECT numero_contrato_temporal FROM contrato WHERE numero_contrato = (SELECT numero_contrato FROM persona));";
-         stmt.executeUpdate(sql);
-         
-         System.out.println("Consultar los contratos proximos a vencer"); 
-         sql = "SELECT numero_contrato, tipo_persona, tipo_uso FROM propietario WHERE fecha_finalizacion > curdate() ORDER BY fecha_finalizacion ASC;\n" +
-"SELECT numero_contrato, seguro_mecanica, fecha_finalizacion, frecuencia_pago, fecha poliza, fecha_terminacion_poliza FROM membresia WHERE fecha_finalizacion > curdate() ORDER BY fecha_finalizacion ASC;\n" +
-"SELECT numero_contrato, fecha_inicio, fecha_finalizacion, frecuencia_pago, poliza_amparo, local_asignado FROM temporal WHERE fecha_finalizacion > curdate() ORDER BY fecha_finalizacion ASC;";
-         stmt.executeUpdate(sql);
-         
-         System.out.println("Registrar el ingreso de un vehiculo autorizado");
-         sql = "INSERT INTO registro (numero_registro, licencia_conductor, cedula_conductor, placa_vehiculo, marca_vehiculo, vol_carga, capacidad_carga ,tipo_vehiculo, tiempo_entrada) VALUES ('1', (SELECT identificacion_conductor, identificador_conductor, placa, marca, volumen, capacidad, tipo FROM vehiculo), GETDATE());";
-         stmt.executeUpdate(sql);
-         
-         System.out.println("Registrar la salida de un vehiculo autorizado");
-         sql = "INSERT INTO registro (numero_registro, retiro_mercancia, tiempo_salida) VALUES ('1', 'y', (SELECT identificacion_conductor FROM vehiculo), GETDATE());";
-         stmt.executeUpdate(sql);
-         
-         System.out.println("consultar los puestos de almacenamiento");
-         sql = "SELECT cantidad_puestos FROM bodega;\n" +
-"SELECT * FROM puesto_almacenamiento;";
-         stmt.executeUpdate(sql);
-         
-         System.out.println("consultar los ingresos y salidas de un vehiculo particular");
-         sql = "SELECT * FROM ingreso WHERE placa_vehiculo = 'abc123';";
-         stmt.executeUpdate(sql);
-         
-         System.out.println("consultar los datos de los clientes por categoria y sus contratos, aqui hay dos formas");
-         sql = "SELECT * FROM persona WHERE numero_contrato_empleo = (SELECT numero_contrato FROM contrato ORDER BY numero_contrato_propietario OR numero_contrato_membresia OR numero_contrato_temporal ASC);\n" +
-"SELECT * FROM persona ORDER BY tipo ASC;";
-         stmt.executeUpdate(sql);
-
-         System.out.println("consultar los vehiculos autorizados para un contrato");
-         sql = "SELECT * FROM vehiculo WHERE numero_contrato = (SELECT numero_contrato FROM CONTRATO;";
-         stmt.executeUpdate(sql);
-         
-         System.out.println("Consultar los vehículos autorizados para un cliente ");
-         sql = "SELECT * FROM vehiculos WHERE identificacion_conductor =(SELECT cedula_conductor FROM registro);";
-         stmt.executeUpdate(sql);
-         
-          System.out.println("consultar los visitantes en una fecha especifica");
-         sql = "SELECT numero_registro, licencia_conductor, cedula_conductor, placa_vehiculo, marca_vehiculo, vol_carga, capacidad_carga ,tipo_vehiculo, tiempo_entrada FROM registro WHERE tiempo_entrada = '2021-10-9';";
-         stmt.executeUpdate(sql);
-         
-         
-      } catch (SQLException e) {
-         e.printStackTrace();
-      } 
-    };
-    
+//    public Consultas(String[] args){
+//        
+//        try(Connection bd_projectfinal = DriverManager.getConnection(DB_URL, USER, PASS); 
+//                Statement stmt = bd_projectfinal.createStatement();
+//        ) {		      
+//         System.out.println("consultar los contratos vigentes de un cliente");          
+//         String sql = "SELECT numero_contrato, tipo_persona, tipo_uso FROM propietario WHERE fecha_finalizacion > curdate() AND numero_contrato = (SELECT numero_contrato_propietario FROM contrato WHERE numero_contrato = (SELECT numero_contrato FROM persona));\n" +
+//"SELECT numero_contrato, seguro_mecanica, fecha_finalizacion, frecuencia_pago, fecha_poliza, fecha_terminacion_poliza FROM membresia WHERE fecha_finalizacion > curdate() AND numero_contrato = (SELECT numero_contrato_membresia FROM contrato WHERE numero_contrato = (SELECT numero_contrato FROM persona));\n" +
+//"SELECT numero_contrato, fecha_inicio, fecha_finalizacion, frecuencia_pago, poliza_amparo, local_asignado FROM membresia WHERE fecha_finalizacion > curdate() AND numero_contrato = (SELECT numero_contrato_temporal FROM contrato WHERE numero_contrato = (SELECT numero_contrato FROM persona));";
+//         stmt.executeUpdate(sql);
+//         
+//         System.out.println("Consultar los contratos proximos a vencer"); 
+//         sql = "SELECT numero_contrato, tipo_persona, tipo_uso FROM propietario WHERE fecha_finalizacion > curdate() ORDER BY fecha_finalizacion ASC;\n" +
+//"SELECT numero_contrato, seguro_mecanica, fecha_finalizacion, frecuencia_pago, fecha poliza, fecha_terminacion_poliza FROM membresia WHERE fecha_finalizacion > curdate() ORDER BY fecha_finalizacion ASC;\n" +
+//"SELECT numero_contrato, fecha_inicio, fecha_finalizacion, frecuencia_pago, poliza_amparo, local_asignado FROM temporal WHERE fecha_finalizacion > curdate() ORDER BY fecha_finalizacion ASC;";
+//         stmt.executeUpdate(sql);
+//         
+//         System.out.println("Registrar el ingreso de un vehiculo autorizado");
+//         sql = "INSERT INTO registro (numero_registro, licencia_conductor, cedula_conductor, placa_vehiculo, marca_vehiculo, vol_carga, capacidad_carga ,tipo_vehiculo, tiempo_entrada) VALUES ('1', (SELECT identificacion_conductor, identificador_conductor, placa, marca, volumen, capacidad, tipo FROM vehiculo), GETDATE());";
+//         stmt.executeUpdate(sql);
+//         
+//         System.out.println("Registrar la salida de un vehiculo autorizado");
+//         sql = "INSERT INTO registro (numero_registro, retiro_mercancia, tiempo_salida) VALUES ('1', 'y', (SELECT identificacion_conductor FROM vehiculo), GETDATE());";
+//         stmt.executeUpdate(sql);
+//         
+//         System.out.println("consultar los puestos de almacenamiento");
+//         sql = "SELECT cantidad_puestos FROM bodega;\n" +
+//"SELECT * FROM puesto_almacenamiento;";
+//         stmt.executeUpdate(sql);
+//         
+//         System.out.println("consultar los ingresos y salidas de un vehiculo particular");
+//         sql = "SELECT * FROM ingreso WHERE placa_vehiculo = 'abc123';";
+//         stmt.executeUpdate(sql);
+//         
+//         System.out.println("consultar los datos de los clientes por categoria y sus contratos, aqui hay dos formas");
+//         sql = "SELECT * FROM persona WHERE numero_contrato_empleo = (SELECT numero_contrato FROM contrato ORDER BY numero_contrato_propietario OR numero_contrato_membresia OR numero_contrato_temporal ASC);\n" +
+//"SELECT * FROM persona ORDER BY tipo ASC;";
+//         stmt.executeUpdate(sql);
+//
+//         System.out.println("consultar los vehiculos autorizados para un contrato");
+//         sql = "SELECT * FROM vehiculo WHERE numero_contrato = (SELECT numero_contrato FROM CONTRATO;";
+//         stmt.executeUpdate(sql);
+//         
+//         System.out.println("Consultar los vehículos autorizados para un cliente ");
+//         sql = "SELECT * FROM vehiculos WHERE identificacion_conductor =(SELECT cedula_conductor FROM registro);";
+//         stmt.executeUpdate(sql);
+//         
+//          System.out.println("consultar los visitantes en una fecha especifica");
+//         sql = "SELECT numero_registro, licencia_conductor, cedula_conductor, placa_vehiculo, marca_vehiculo, vol_carga, capacidad_carga ,tipo_vehiculo, tiempo_entrada FROM registro WHERE tiempo_entrada = '2021-10-9';";
+//         stmt.executeUpdate(sql);
+//         
+//         
+//      } catch (SQLException e) {
+//         e.printStackTrace();
+//      } 
+//    };
+//    
 
     
     /*
@@ -118,7 +118,7 @@ public class Consultas {
         mysql.executeQuery(comando);  // devuelve un set, por ende debe parsearse la entrada
         
         
-        returnValue = "Consultar contratos vigentes";
+        returnValue = ProccessQuerieResult(comando);
         
         return returnValue;
     }
